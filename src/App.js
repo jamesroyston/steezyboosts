44fd0f3c8fbc035f9fc70a4aa5eaa258395628a1
James Royston III <jamesroyston03@gmail.com>
2020-12-18 16:54:25 -0600

    add Home component and import reactplayer

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import './App.css';
-import React, { useEffect } from 'react'
-import example from "./utils/example"
-
+import React from 'react'
+import Home from "./components/Home"
 
 function App() {
-  // this just runs side effects or functions once the component mounts/loads for the first time
-  useEffect(() => {
-
-    example()
-  }, [])
   return (
-    <div className="App">
-
+    <div>
+     <Home />
     </div>
   );
 }
---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import styled from 'styled-components';
+import ReactPlayer from 'react-player';
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  overflow: hidden;
+  background-color: #E5E5E5;
+`;
+
+const Cta = styled.button`
+  
+`
+
+const Home = () => {
+    return <Container>
+    </Container>
+}
+
+export default Home;
